256a8268b376e2e72556c43901ff6bb92cb4119a
Mathias-ao <109521141+Mathias-ao@users.noreply.github.com>
2026-09-02 22:31:27 +0200

    feat: export membership commands

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import { onCall } from "firebase-functions/v2/https";
 import { requireLeaguePlayer } from "./auth/authorization.js";
 
+export { requestLeagueMembership } from "./commands/players/requestMembership.js";
+export { adminSetMembershipStatus } from "./commands/players/setMembershipStatus.js";
 export { adminCreateSeason } from "./commands/seasons/createSeason.js";
 
 export const backendHealth = onCall(async (request) => {
@@ -10,6 +12,6 @@ export const backendHealth = onCall(async (request) => {
     ok: true,
     service: "league-of-friends-backend",
     playerId: actor.playerId,
-    role: actor.role
+    role: actor.role,
   };
 });
