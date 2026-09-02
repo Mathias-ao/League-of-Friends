d7b47ce3db0f46693f0c66a8b10ca5b2c3c82bea
Mathias-ao <109521141+Mathias-ao@users.noreply.github.com>
2026-09-02 21:09:51 +0200

    feat: export season creation command

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import { onCall } from "firebase-functions/v2/https";
 import { requireLeaguePlayer } from "./auth/authorization.js";
 
+export { adminCreateSeason } from "./commands/seasons/createSeason.js";
+
 export const backendHealth = onCall(async (request) => {
   const actor = await requireLeaguePlayer(request);
 
