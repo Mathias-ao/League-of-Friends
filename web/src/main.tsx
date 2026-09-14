6901a244061086f667819f44df88facb3baa0910
Mathias-ao <109521141+Mathias-ao@users.noreply.github.com>
2026-09-14 17:01:27 +0200

    Fix Cinzel font import found by recovered website CI

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import '@fontsource/cinzel/500.css';
+import '@fontsource/cinzel/400.css';
 import '@fontsource/cinzel/600.css';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
