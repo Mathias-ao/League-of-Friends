7826bc2b212dad1392d278ea7e2d1ffaf80bd131
Mathias-ao <109521141+Mathias-ao@users.noreply.github.com>
2026-09-16 10:19:46 +0200

    feat: export TownBell launch stats ingestion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,6 +23,7 @@ export { respondToGameResult } from "./commands/results/respondToGameResult.js";
 export { adminResolveGameResult } from "./commands/results/adminResolveGameResult.js";
 export { disputeCanonicalGameResult } from "./commands/results/disputeCanonicalGameResult.js";
 export { adminResolveCanonicalResultDispute } from "./commands/results/adminResolveCanonicalResultDispute.js";
+export { adminIngestTownBellStats } from "./commands/statistics/ingestTownBellStats.js";
 export { adminIngestReplayStats } from "./commands/statistics/ingestReplayStats.js";
 export { adminProcessReplayDerivedStats } from "./commands/statistics/processReplayDerivedStats.js";
 export { adminProcessReplayAnalysis } from "./commands/statistics/processReplayAnalysis.js";
